3a32dea4c400cfb67279c5efb8b2a076989c5ba2
warren <hwhong1129@gmail.com>
2020-05-27 22:16:09 -0700

    center the canvas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
+import { Toolbar } from "./component/toolbar";
+import styles from "./app.module.css";
 const ENDPOINT = "http://127.0.0.1:4001";
 const socket = socketIOClient(ENDPOINT);
 
@@ -137,7 +139,13 @@ function App() {
 
   return (
     <>
-      <canvas ref={canvasRef} width={800} height={800} />
+      <Toolbar />
+      <canvas
+        ref={canvasRef}
+        width={800}
+        height={600}
+        className={styles.root}
+      />
     </>
   );
 }
--- frontend/src/component/toolbar.tsx
+++ frontend/src/component/toolbar.tsx
@@ -4,9 +4,9 @@ import styles from "./toolbar.module.css";
 export function Toolbar() {
   return (
     <div className={styles.root}>
-      <div className={styles.left}></div>
-      <div className={styles.center}></div>
-      <div className={styles.right}></div>
+      <div className={styles.leftContent}></div>
+      <div className={styles.centerContent}></div>
+      <div className={styles.rightContent}></div>
     </div>
   );
 }
